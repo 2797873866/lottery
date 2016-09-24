5833b0c57d68d488bb81183494196bdaf136cd80
李建峰 <lijianfeng@macbook-pro.lan>
2016-09-24 23:59:54 +0800

    项目框架完成

---
+++ 彩票项目/彩票项目/Classce/main/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  彩票项目
+//
+//  Created by ljf on 16/8/18.
+//  Copyright © 2016年 LJF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 彩票项目/彩票项目/Classce/main/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  彩票项目
+//
+//  Created by ljf on 16/8/18.
+//  Copyright © 2016年 LJF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
